fc59a199eef52d8f7df903a5f6702649d5b41766
ilyasBozdemir <Bozdemir.ib70@gmail.com>
2022-07-21 12:50:01 +0300

    project uploaded

---
+++ picture_zoom/picture_zoom/PictureViewForm.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace picture_zoom
+{
+    partial class PictureViewForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(23, 85);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(353, 562);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseEnter += new System.EventHandler(this.pictureBox1_MouseEnter);
+            this.pictureBox1.MouseLeave += new System.EventHandler(this.pictureBox1_MouseLeave);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(23, 28);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(553, 493);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(100)))), ((int)(((byte)(0)))));
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Location = new System.Drawing.Point(434, 85);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(600, 562);
+            this.panel1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Corbel", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(399, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(261, 55);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "select an image";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1060, 684);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Picture Zoom";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ picture_zoom/picture_zoom/PictureViewForm.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace picture_zoom
+{
+    public partial class PictureViewForm : Form
+    {
+        public PictureViewForm()
+        {
+            InitializeComponent();
+        }
+        PictureBox ZoomPictureBox;
+
+        Graphics GraphicCapture;
+        Bitmap TemporalImage;
+        bool isPictureBoxIn;
+        int Zoom = 2; // 1px, 2px, 3px ....
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ZoomPictureBox = new PictureBox
+            {
+                Size = new System.Drawing.Size(100, 100),
+            };
+            timer1.Start();
+            pictureBox1.MouseWheel += PictureBox1_MouseWheel;
+            
+        }
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            ZoomPictureBox.Location = new System.Drawing.Point
+            {
+                X = e.X - (ZoomPictureBox.Size.Width / 2),//mouse center position of X
+                Y = e.Y - (ZoomPictureBox.Size.Height / 2),//mouse center position of X
+            };
+        }
+
+        private void pictureBox1_MouseLeave(object sender, EventArgs e)
+        {
+            isPictureBoxIn = false;
+            Cursor = Cursors.Default;
+            //pictureBox1.Controls.Remove(ZoomPictureBox);
+        }
+
+        private void pictureBox1_MouseEnter(object sender, EventArgs e)
+        {
+            isPictureBoxIn = true;
+            Cursor = Cursors.Cross;
+            //pictureBox1.Controls.Add(ZoomPictureBox);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //
+            int mouseX = MousePosition.X;
+            int mouseY = MousePosition.Y;
+            //
+            int imageW = pictureBox2.Width;
+            int imageH = pictureBox2.Height;
+            if (isPictureBoxIn)
+            {
+                TemporalImage = new Bitmap(imageW / Zoom, imageH / Zoom, System.Drawing.Imaging.PixelFormat.Format64bppPArgb);
+                GraphicCapture = this.CreateGraphics();
+
+                GraphicCapture = Graphics.FromImage(TemporalImage);
+                GraphicCapture.CopyFromScreen(mouseX - imageW / (Zoom * 2), mouseY - imageH / (Zoom * 2), 0, 0, pictureBox1.Size);
+                //
+                Bitmap newImage = new Bitmap(imageW, imageH);
+                GraphicCapture = Graphics.FromImage(newImage);
+                GraphicCapture.SmoothingMode = SmoothingMode.HighQuality;
+                GraphicCapture.DrawImage(TemporalImage, new Rectangle(0, 0, imageW, imageH));
+                pictureBox2.Image = newImage;
+
+                //
+                Rectangle rect = new Rectangle(0, 0, imageW, imageH);
+                GraphicsPath path = new GraphicsPath();
+                path.AddEllipse(rect);
+                pictureBox2.Region = new Region(path);
+                //
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if ((e.KeyCode & Keys.Add) == Keys.Add)
+            {
+                Zoom++;
+            }
+            if ((e.KeyCode & Keys.Separator) == Keys.Separator)
+            {
+                if (Zoom > 2)
+                {
+                    Zoom--;
+                }
+            }
+            if ((e.KeyCode & Keys.Escape) == Keys.Escape)
+            {
+                this.Close();
+            }
+        }
+        private void PictureBox1_MouseWheel(object sender, MouseEventArgs e)
+        {
+            if (e.Delta >= 0)
+                Zoom++;
+            else
+            if (Zoom > 2)
+            {
+                Zoom--;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.AddExtension = false;
+            openFileDialog.CheckFileExists = true;
+            openFileDialog.CheckPathExists = true;
+            openFileDialog.Filter = "Supported Image File|*.jpg;*.jpeg;*.bmp;*.png;*.dib;*.gif";
+            openFileDialog.InitialDirectory = System.Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+            openFileDialog.Multiselect = false;
+            openFileDialog.ReadOnlyChecked = false;
+            openFileDialog.ShowHelp = true;
+            openFileDialog.ShowReadOnly = false;
+            openFileDialog.SupportMultiDottedExtensions = true;
+            openFileDialog.Title = "Select an image...";
+            openFileDialog.ValidateNames = true;
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image = new Bitmap(openFileDialog.FileName);
+            }
+        }
+    }
+}
